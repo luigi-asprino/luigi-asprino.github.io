bd84e6140923acb022f855209648f12b0049e4cc
Luigi Asprino <luigi.asprino@gmail.com>
2019-07-23 12:24:24 +0200

    new website

---
+++ js/populatePublications.js
@@ -0,0 +1,73 @@
+$.ajaxSetup({
+	beforeSend : function(xhr) {
+		if (xhr.overrideMimeType) {
+			xhr.overrideMimeType("application/json");
+		}
+	}
+});
+$.getJSON("./js/publications_data.json", function(data) {
+
+	for ( var k in data) {
+		
+		var accordionElem = document.getElementById("accordionPub");
+		var cardElement = document.createElement('div');
+		cardElement.setAttribute('class', 'card');
+
+		cardElement.innerHTML = getHeaderDiv(data[k].year)
+				+ getCollapseDiv(data[k].year, data[k].publications);
+
+		accordionElem.appendChild(cardElement);
+	}
+
+});
+
+function getHeaderDiv(year) {
+	return "<div class=\"card-header\" id=\"heading"
+			+ year
+			+ "\"> <h5 class=\"mb-0\"><button class=\"btn btn-year\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapse"
+			+ year + "\"aria-expanded=\"true\" aria-controls=\"collapse" + year
+			+ "\">" + year + "</button></h5></div>";
+}
+
+function getCollapseDiv(year, pubs) {
+	var s = "<div id=\"collapse" + year
+			+ "\" class=\"collapse\" aria-labelledby=\"heading" + year
+			+ "\" data-parent=\"#accordionPub\"> <div class=\"card-body\">";
+
+	pubs.forEach(function(e, i) {
+		s = s + addPub(e.id, i+1, e.authors, e.title, e.inField,e.doi,e.bibtex,e.url);
+	});
+
+	s = s + "</div></div>";
+
+	return s;
+}
+
+function addPub(id, num, aurhorsListHTML, title, inField, doi, bibtex, url) {
+	var s = "<p>" + num + ". " + aurhorsListHTML;
+
+	if (url != null) {
+		s = s + ". <a href=\"" + url + "\">" + title + "</a>. ";
+	} else {
+		s = s + ". " + title + ". ";
+	}
+
+	s = s + " In: <i>" + inField + "</i>";
+
+	if (doi != null) {
+		s = s + " DOI: <a href=\"https://doi.org/" + doi + "\">" + doi
+				+ "</a> ";
+	}
+
+	s = s
+			+ " <button class=\"btn btn-bibtex \" type=\"button\" data-toggle=\"collapse\" href=\"#collapseBib"
+			+ id
+			+ "\" role=\"button\" aria-expanded=\"false\" aria-controls=\"collapseBib"
+			+ id + "\">bibtex</button></p>";
+
+	s = s + "<div class=\"collapse\" id=\"collapseBib" + id
+			+ "\"><div class=\"card card-body\">" + bibtex + "</div></div>";
+
+	return s;
+
+}
